077ebec8a42667fcbae007e35a07fb64ef78468d
KatIsHere <dubska.kate@gmail.com>
2018-07-09 19:15:38 +0300

    Update LinearEquations.h

--- LinearEquations.h
+++ LinearEquations.h
@@ -1,5 +1,8 @@
 #pragma once
-double* GaussMethod(double** A, double* b, const int& m);
-double* GaussMethod(int** A, int* b, const int& m);
+double* GaussMethod(double** A,double* b, const int& m);
 double* TridiagonalSolve(const double *a, const double *b, double *c, double *d, const int& n);
 double* TridiagonalSolve(const int *a, const int *b, double *c, int *d, const int& n);
+int LUPDecompose(double **A, const int& N, double Tol, int *P);
+void LUPSolve(double **A, int *P, double *b, const int& N, double *x);
+void LUPInvert(double **A, int *P, const int& N, double **IA);
+double LUPDeterminant(double **A, int *P, const int& N);
